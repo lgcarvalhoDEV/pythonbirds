baab1e00a6c30e70894a0826b916d0d17b72189e
Lucas G Carvalho <carvalhol.amt@gmail.com>
2020-09-10 14:27:33 -0300

    Criado atributo de classe: olhos.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,10 @@
-class Pessoa:
+class Pessoa: #definição de Classe
+    olhos = 2 # -Atributo default (atributo de classe) - criá-lo quando o valor será o mesmo para todos os objetos. Se não, criar como atributo de instância (INIT).
+
     def __init__(self, *filhos, nome=None, idade=27):
-        self.idade = idade
-        self.nome = nome
-        self.filhos = list(filhos)
+        self.idade = idade # -Atributo de dado
+        self.nome = nome # -Atributo de dado
+        self.filhos = list(filhos) # -Atributo complexo - quantidade variável
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
@@ -17,7 +19,14 @@ if __name__ == '__main__':
     print(lucas.idade)
     for filho in lucas.filhos:
         print(filho.nome)
-    lucas.sobrenome = 'Carvalho'
-    del lucas.filhos
-    print(lucas.__dict__)
+    lucas.sobrenome = 'Carvalho' # -Inserindo atributo dinâmico - em tempo de execução (Não definido no método D.under init) - não aconselhável
+    del lucas.filhos # -Deletando atributo em tempo de execução (não aconselhável)
+    lucas.olhos = 1
+    del lucas.olhos #apagando o atributo dinânico do objeto e tornando-o default
+    print(lucas.__dict__) # -Atributo especial para conferir atributos de instância e apenas instância e nao classe (criados no D.under init e também dinamicamente)
     print(jamile.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos) # -Acessando o att através da classe Pessoa
+    print(jamile.olhos) # -Acessando o att através do objeto
+    print(lucas.olhos)
+    print(id(Pessoa.olhos), id(lucas.olhos), id(jamile.olhos))
