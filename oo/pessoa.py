ade129110e56f10bba05ddc4795dd53a57025cc0
Lucas G Carvalho <carvalhol.amt@gmail.com>
2020-09-10 13:08:13 -0300

    Criados atributos de instância: nome e idade.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,17 @@
 class Pessoa:
-    pass
+    def __init__(self, nome=None, idade=27):
+        self.idade = idade
+        self.nome = nome
+
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa('Jamile')
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Lucas'
+    print(p.nome)
+    print(p.idade)
