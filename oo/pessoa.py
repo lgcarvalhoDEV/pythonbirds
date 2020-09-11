2b8ec44066cdf3ceac3e3b2ae9cbbe1c78c8f13b
Lucas G Carvalho <carvalhol.amt@gmail.com>
2020-09-11 14:06:35 -0300

    Adicionado o Comando super() para sobrescrita de método - acessa sempre a classe pai, no caso de homem é pessoa.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -7,19 +7,28 @@ class Pessoa: #definição de Classe
         self.filhos = list(filhos) # -Atributo complexo - quantidade variável
 
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá, meu nome é {self.nome}.'
 
-    # exemplo: @staticmethod # -Utilização do decorator
-    #          def metodo_estatico():
-    #              return 42
+    @staticmethod # -Utilização do decorator
+    def metodo_estatico():
+        return 42
 
-    # exemplo: @classmethod
-    #          def nome_e_atributos_de_classe(cls):
-    #              return f'{cls} - olhos {cls.olhos}'
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
+class Homem(Pessoa):
+    def cumprimentar(self):
+        cumprimentar_da_classe_pai= super().cumprimentar() #Comando super() - sobrescrita de método - acessa sempre a classe pai, no caso de homem é pessoa.
+        return f'{cumprimentar_da_classe_pai} (Aperto de mão)'
+
+class Mutante(Pessoa): #classe pai (Pessoa) - Herança
+    olhos = 3 #Sobrescrita de atributo
+
 
 if __name__ == '__main__':
-    jamile = Pessoa(nome='Jamile')
-    lucas = Pessoa(jamile, nome='Lucas')
+    jamile = Mutante(nome='Jamile')
+    lucas = Homem(jamile, nome='Lucas')
     print(Pessoa.cumprimentar(lucas))
     print(id(lucas))
     print(lucas.cumprimentar())
@@ -33,10 +42,18 @@ if __name__ == '__main__':
     del lucas.olhos #apagando o atributo dinânico do objeto e tornando-o default
     print(lucas.__dict__) # -Atributo especial para conferir atributos de instância e apenas instância e nao classe (criados no D.under init e também dinamicamente)
     print(jamile.__dict__)
-    Pessoa.olhos = 3
     print(Pessoa.olhos) # -Acessando o att através da classe Pessoa
     print(jamile.olhos) # -Acessando o att através do objeto
     print(lucas.olhos)
     print(id(Pessoa.olhos), id(lucas.olhos), id(jamile.olhos))
     #   print(Pessoa.metodo_estatico(), lucas.metodo_estatico())
     #   print(Pessoa.nome_e_atributos_de_classe(), lucas.nome_e_atributos_de_classe())
+    pessoa = Pessoa('Anonimo')
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(pessoa, Homem))
+    print(isinstance(lucas, Homem))
+    print(isinstance(jamile, Homem))
+    print(isinstance(jamile, Pessoa))
+    print(lucas.olhos) #Voltará a resposta de 3 olhos, pois lucas entrou na classe de mutante.
+    print(jamile.cumprimentar())
+    print(lucas.cumprimentar())
